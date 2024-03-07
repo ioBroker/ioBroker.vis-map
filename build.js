fc7214a5b39e2b3a911c0558efaab4b6ccebcf51
GermanBluefox <dogafox@gmail.com>
2024-03-07 12:37:33 +0800

    - (bluefox) Removed dependency to `iobroker.vis`. Only show the message if vis is not installed

--- build.js
+++ build.js
@@ -4,7 +4,10 @@ const fs = require('node:fs');
 const pack = require('./package.json');
 
 const html = fs.readFileSync(`${__dirname}/widgets/map.html`, 'utf8');
-const newHtml = html.replace(/version: "\d+\.\d+\.\d+"/, `version: "${pack.version}"`);
+const newHtml = html
+    .replace(/version: "\d+\.\d+\.\d+"/, `version: "${pack.version}"`)
+    .replace(/version: "\d+\.\d+\.\d+",/, `version: "${pack.version}",`)
+    .replace(/version: '\d+\.\d+\.\d+',/, `version: '${pack.version}',`);
 if (html !== newHtml) {
     fs.writeFileSync(`${__dirname}/widgets/map.html`, newHtml);
     console.log('map.html updated');
